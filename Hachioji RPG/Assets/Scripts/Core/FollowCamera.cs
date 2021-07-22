5d2882bb2fede6fc89688cdbd7a37f452f29667c
ueharajohji <itsukiuehara@protonmail.com>
2021-07-22 19:58:26 +0900

    added movement function using raytracing

---
+++ Hachioji RPG/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // Update is called once per frame
+    [SerializeField] Transform target;
+    private void LateUpdate()
+    {
+        GetComponent<Transform>().position = target.position;
+    }
+}
