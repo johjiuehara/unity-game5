f8d08080e592bfeef774ff34d35e0fef4b319fe2
ueharajohji <itsukiuehara@protonmail.com>
2021-07-23 22:43:46 +0900

    Added Action Scheduler and Interface

--- Hachioji RPG/Assets/Scripts/Combat/Fighter.cs
+++ Hachioji RPG/Assets/Scripts/Combat/Fighter.cs
@@ -1,15 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 2f;
+        Transform target;
+
+        private void Update()
         {
-            print("Take that you short, squat peasant!");
+            if (!target) return;
+            if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Cancel();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this); 
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
         }
     }
 }
