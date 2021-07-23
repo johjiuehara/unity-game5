5263629f21bf071100c5dfd109c0c88e086ef61e
ueharajohji <itsukiuehara@protonmail.com>
2021-07-24 03:29:34 +0900

    Added Nested Prefabs and Enemy Animations

--- Hachioji RPG/Assets/Scripts/Combat/Fighter.cs
+++ Hachioji RPG/Assets/Scripts/Combat/Fighter.cs
@@ -10,10 +10,14 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (!target) return;
             if (!GetIsInRange())
             {
@@ -22,7 +26,27 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                // this will trigger the Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        /**
+         * Animation Event
+         * **/
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
